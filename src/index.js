2c1232870099e5a80638602e1be988413d746a4a
KEVIN RAMIREZ <kevin.ramirez1996.gmail.com>
2017-02-24 11:05:26 -0500

    Config firebase

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase';
 import App from './App';
 import './index.css';
+import env from '../env.json';
+ 
+firebase.initializeApp({
+	apiKey: env.apiKey,
+    authDomain: env.authDomain,
+    databaseURL: env.databaseURL,
+    storageBucket: env.storageBucket,
+    messagingSenderId: env.messagingSenderId
+});
 
 ReactDOM.render(
   <App />,
